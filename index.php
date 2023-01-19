018e7ab25cf2514b3693def793d60ed65ee083b4
lajang3 <lajangwicaksono3@gmail.com>
2023-01-19 12:56:58 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testing2</title>
+</head>
+<body>
+    <h1>yahallo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi ad minus porro architecto quaerat quos beatae quod eveniet sunt labore inventore, vel recusandae magnam obcaecati dolore animi? Animi, illo ducimus.</p>
+</body>
+</html>
